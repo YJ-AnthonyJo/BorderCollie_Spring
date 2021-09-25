5666b43fcb951cbf2e311f1aa396496fd11c04ae
yj.anthony <yj.anthonyjo@gmail.com>
2021-09-25 14:33:54 +0900

    초기세팅

---
+++ src/main/java/com/example/BorderCollie/BorderCollieApplication.java
@@ -0,0 +1,13 @@
+package com.example.BorderCollie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorderCollieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorderCollieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BorderCollie/BorderCollieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BorderCollie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorderCollieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
